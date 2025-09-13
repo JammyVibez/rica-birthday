1de337823aed1db19f1ff53951f3563ed7671d03
Anonymous-dev384 <anonymousme761@gmail.com>
2025-09-13 03:03:19 +0100

    fix display

--- server/index.ts
+++ server/index.ts
@@ -52,6 +52,18 @@ app.use((req, res, next) => {
 
   const server = await registerRoutes(app);
 
+  if (!isProduction) {
+    await setupVite(app, server);
+  } else {
+    serveStatic(app);
+  }
+
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, "0.0.0.0", () => {
+    log(`Server running on http://0.0.0.0:${PORT}`);
+  });
+})(););
+
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
